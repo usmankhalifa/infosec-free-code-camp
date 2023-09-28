d5c875b8f888f29d950ecdffaba34ac22416f4de
usmankhalifa <usmankhalifa10@outlook.com>
2023-09-28 14:19:19 +0100

    message

--- myApp.js
+++ myApp.js
@@ -2,11 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
-app.listen(PORT), () => {
-  console.log(`server started on port ${PORT}`);
-}
-
+app.use(helmet.hidePoweredBy());
 
 
 
@@ -61,7 +57,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 10000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`server started on port ${port}`);
 });
