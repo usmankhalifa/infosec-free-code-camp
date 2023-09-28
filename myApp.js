06bcac26899ae6cdd76f4c685db2fea08041b81c
usmankhalifa <usmankhalifa10@outlook.com>
2023-09-28 10:10:27 +0100

    Hide powered by

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT), () => {
+  console.log(`server started on port ${PORT}`);
+}
 
 
 
