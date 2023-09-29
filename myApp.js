c30f4e01eb6f9816297e0302523efb73f2e47bed
usmankhalifa <usmankhalifa10@outlook.com>
2023-09-29 10:52:33 +0100

    message

--- myApp.js
+++ myApp.js
@@ -13,10 +13,10 @@ app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({
+app.use(helmet.contentSecurityPolicy({directives: {
   defaultSrc: ["'self'"],
   scriptSrc: ["'self'", 'trusted-cdn.com']
-}));
+}}));
 
 
 
