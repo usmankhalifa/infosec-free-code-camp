527ac6cc1c766be744ecabcb063b5d9609c2ef24
usmankhalifa <usmankhalifa10@outlook.com>
2023-09-29 10:24:41 +0100

    message

--- myApp.js
+++ myApp.js
@@ -8,6 +8,8 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
+var ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 
 
 
